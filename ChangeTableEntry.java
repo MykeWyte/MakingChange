d39768b7054fee44eb7747800dcfb9d8801ae35a
Michael White <53944993+MykeWyte@users.noreply.github.com>
2022-10-06 13:57:40 -0400

    updated to use purses
    instead of previous linked list structure

--- ChangeTableEntry.java
+++ ChangeTableEntry.java
@@ -1,16 +1,28 @@
 package MakeChange;
 
 /**
+ * This class serves as an entry recording coin information in 
+ * the making change problem
+ * 
+ * @author Matthew Jacobs, Michael White
+ * @version 1.0.0
+ * file: ChangeTableEntry.java
+ * Created October 2022
+ * Copyright Matthew Jacobs, Michael White
  *
- * @author swocc
- */
+ * Description: This class serves as a data structure to store information
+ * needed to solve the making change problem with recursion or dynamic
+ * programming.
+ * It contains a monetary value, a number of coins and an array containing all 
+ * the stored coins
+ * */
 public class ChangeTableEntry {
     // variables
     
     private int val; // total value of change made
-    private int numCoins;
-    private int prevDenom;
-    private ChangeTableEntry prev; // previous change table entry in recursion
+    private int numCoins; // could be replaced with an O(n) method, but is 
+    // easier to simply track
+    private int[] purse; // previous change table entry in recursion
     
     // constructor
     
@@ -20,12 +32,11 @@ public class ChangeTableEntry {
      * @param newPrevDenom;
      * @param newNumCoins
      */
-    public ChangeTableEntry(int newVal, int newNumCoins, int newPrevDenom, ChangeTableEntry newPrev)
+    public ChangeTableEntry(int newVal, int newNumCoins, int[] newPurse)
     {
         val = newVal;
         numCoins = newNumCoins;
-        prevDenom = newPrevDenom;
-        prev = newPrev;
+        purse = newPurse;
     }
     
     // Methods
@@ -37,18 +48,13 @@ public class ChangeTableEntry {
         return val;
     }
     
-    public int getPrevDenom()
-    {
-        return prevDenom;
-    }
-    
     public int getNumCoins()
     {
         return numCoins;
     }
     
-    public ChangeTableEntry getPrev()
+    public int[] getPurse()
     {
-        return prev;
+        return purse;
     }
 }
